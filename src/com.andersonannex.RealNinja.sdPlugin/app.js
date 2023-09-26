bcedcf635eddcaf1dae033fdfe04c43bedbe3a8c
Scott Anderson <perfect_disturbance@hotmail.com>
2023-09-26 08:51:14 -0600

    First version of toggle no payload or method.

--- src/com.andersonannex.RealNinja.sdPlugin/app.js
+++ src/com.andersonannex.RealNinja.sdPlugin/app.js
@@ -52,4 +52,46 @@ apiAction.onKeyUp(async ({ action, context, device, event, payload }) => {
 	}
 });
 
+apiToggleAction.onWillAppear((jsn) => {
+	$SD.getSettings(jsn.context);
+});
+
+apiToggleAction.onDidReceiveSettings((jsn) => {
+		//saveSettings to the cache
+		if(!settingsCache.hasOwnProperty(jsn.context)) {
+			settingsCache[jsn.context] = {};
+		}
+		settingsCache[jsn.context].onurl = jsn.payload.settings.onurl;
+		settingsCache[jsn.context].offurl = jsn.payload.settings.offurl;
+		settingsCache[jsn.context].method = jsn.payload.settings.method;
+		settingsCache[jsn.context].reqpayload = jsn.payload.settings.reqpayload;
+		settingsCache[jsn.context].successfield = jsn.payload.settings.successfield;
+	}
+); 
+
+apiToggleAction.onKeyUp(async ({ action, context, device, event, payload }) => {
+	if(payload.state == 0) {
+		console.log("State 0 -> 1");
+		var response = await getURL(settingsCache[context].onurl);
+		console.log(response);
+		if(!response[settingsCache[context].successfield]) {
+			// We failed so prevent state change and show alert.
+			$SD.setState(context, payload = { state: 1 });
+			$SD.showAlert(context);
+		}
+	} else if(payload.state == 1) {
+		console.log("State 1 -> 0");
+		var response = await getURL(settingsCache[context].offurl);
+		console.log(response);
+		if(!response[settingsCache[context].successfield]) {
+			// We failed so prevent state change and show alert.
+			$SD.setState(context, payload = { state: 0 });
+			$SD.showAlert(context);
+		}
+
+	} else {
+		console.log("Invalid State Transition");
+		$SD.showAlert(context);
+	}
+});
 
