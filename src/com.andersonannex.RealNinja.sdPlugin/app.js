ea00d9474a87b2037af1ed2c8410b37b199a40af
Scott Anderson <perfect_disturbance@hotmail.com>
2023-09-26 12:36:51 -0600

    Added initial version of text response display.

--- src/com.andersonannex.RealNinja.sdPlugin/app.js
+++ src/com.andersonannex.RealNinja.sdPlugin/app.js
@@ -1,8 +1,16 @@
 /// <reference path="libs/js/action.js" />
 /// <reference path="libs/js/stream-deck.js" />
 
+/****************************************************
+* TODO: implement payload and post capability
+* 		Resolve errors if settings like successfeild or message feild are not set.
+*		Maybe make it so toggle monitors state
+*		Do something else really cool
+****************************************************/
+
 const apiAction = new Action('com.andersonannex.realninja.apiaction');
 const apiToggleAction = new Action('com.andersonannex.realninja.apitoggleaction');
+const apiTextAction = new Action('com.andersonannex.realninja.apitextaction');
 var settingsCache = {};
 
 async function getURL(url) {
@@ -52,6 +60,36 @@ apiAction.onKeyUp(async ({ action, context, device, event, payload }) => {
 	}
 });
 
+apiTextAction.onWillAppear((jsn) => {
+	$SD.getSettings(jsn.context);
+});
+
+apiTextAction.onDidReceiveSettings((jsn) => {
+		//saveSettings to the cache
+		if(!settingsCache.hasOwnProperty(jsn.context)) {
+			settingsCache[jsn.context] = {};
+		}
+		settingsCache[jsn.context].url = jsn.payload.settings.url;
+		settingsCache[jsn.context].method = jsn.payload.settings.method;
+		settingsCache[jsn.context].reqpayload = jsn.payload.settings.reqpayload;
+		settingsCache[jsn.context].successfield = jsn.payload.settings.successfield;
+		settingsCache[jsn.context].messagefield = jsn.payload.settings.messagefield;
+	}
+); 
+
+apiTextAction.onKeyUp(async ({ action, context, device, event, payload }) => {
+	var response = await getURL(settingsCache[context].url);
+	console.log(response);
+	if(response[settingsCache[context].successfield]) {
+		var str = response[settingsCache[context].messagefield];
+		message = str.charAt(0).toUpperCase() + str.slice(1);
+		$SD.setTitle(context, message);
+		//TODO: Maybe add timeout to reset the text.
+	} else {
+		$SD.showAlert(context);
+	}
+});
+
 apiToggleAction.onWillAppear((jsn) => {
 	$SD.getSettings(jsn.context);
 });
@@ -71,7 +109,6 @@ apiToggleAction.onDidReceiveSettings((jsn) => {
 
 apiToggleAction.onKeyUp(async ({ action, context, device, event, payload }) => {
 	if(payload.state == 0) {
-		console.log("State 0 -> 1");
 		var response = await getURL(settingsCache[context].onurl);
 		console.log(response);
 		if(!response[settingsCache[context].successfield]) {
@@ -80,7 +117,6 @@ apiToggleAction.onKeyUp(async ({ action, context, device, event, payload }) => {
 			$SD.showAlert(context);
 		}
 	} else if(payload.state == 1) {
-		console.log("State 1 -> 0");
 		var response = await getURL(settingsCache[context].offurl);
 		console.log(response);
 		if(!response[settingsCache[context].successfield]) {
